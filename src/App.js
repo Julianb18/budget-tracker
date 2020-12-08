0f70a2f2927bd6e9580120c8bce87e3da6f41e68
Julianb18 <julian141718@gmail.com>
2020-12-08 13:04:27 +0100

    standared css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import { Header } from "./components/Header";
 
 function App() {
-  return <div className="App">A new start</div>;
+  return (
+    <div className="container">
+      <div className="app-wrapper">
+        <Header />
+      </div>
+    </div>
+  );
 }
 
 export default App;
