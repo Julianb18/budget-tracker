18d902a49664af5c47a897b4b3de1eec36cc4164
Julianb18 <julian141718@gmail.com>
2020-12-08 13:23:33 +0100

    transaction component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import { Header } from "./components/Header";
 import { Balance } from "./components/Balance";
 import { AddTransaction } from "./components/AddTransaction";
+import { IncomeList } from "./components/IncomeList";
+import { ExpenseList } from "./components/ExpenseList";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Header />
         <Balance />
         <AddTransaction />
+        <IncomeList />
+        <ExpenseList />
       </div>
     </div>
   );
