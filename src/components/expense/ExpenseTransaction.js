7e12417d63b411761ace8137e626280bdd7c3d20
Julianb18 <julian141718@gmail.com>
2020-12-09 13:38:28 +0100

    basic project finished

--- src/components/expense/ExpenseTransaction.js
+++ src/components/expense/ExpenseTransaction.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../../context/GlobalState";
 
 export const ExpenseTransaction = ({ transaction }) => {
+  const { deleteTransaction } = useContext(GlobalContext);
+
   return (
     <li className="transaction">
       <span className="transaction-text">{transaction.expenseText}</span>
       <span className="transaction-amount">€{transaction.expenseAmount}</span>
-      <button className="delete-btn">
+      <button
+        className="delete-btn"
+        onClick={() => deleteTransaction(transaction.id)}
+      >
         <i className="fas fa-trash"></i>
       </button>
     </li>
